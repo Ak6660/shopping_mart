aa0250971d9eed2f7d6e7ffe338df59a5cd3fca6
Ak6660 <ayushtyagi4971@gmail.com>
2024-03-08 18:08:24 +0530

    password reveal

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -68,6 +68,7 @@ const SignIn = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
+          <p>Please Enter admin-96 as username and password</p>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Sign in to your account
           </h2>
@@ -170,6 +171,8 @@ const SignIn = () => {
             </button>
           </div>
         </form>
+
+        <p>This website is for demostration purpose only</p>
       </div>
     </div>
   );
